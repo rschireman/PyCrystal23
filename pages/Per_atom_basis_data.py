de5e4d78481b26d02929f12b9e480fadd84ec2f7
Ray Schireman <41241487+rschireman@users.noreply.github.com>
2024-06-24 20:54:10 -0500

    initial

--- pages/Per_atom_basis_data.py
+++ pages/Per_atom_basis_data.py
@@ -1,6 +1,14 @@
-import streamlit_material
+import streamlit as st
+import basis_set_exchange as bse
 
-event = streamlit_material.st_material_search_bar_components(
-  key="search-bar", placeholder="e.g. H,O"
-)
+st.set_page_config(
+        page_title="CRYSTAL Basis Sets",
+        page_icon='U+269B',
+        layout='wide',
+        initial_sidebar_state='expanded'
+    )
 
+st.title('CRYSTAL Basis Sets')
+
+all_basis_sets = bse.get_all_basis_names()
+user_basis_set = st.sidebar.selectbox("Basis Set", all_basis_sets, index=33)
