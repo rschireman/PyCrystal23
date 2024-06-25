1ad5f24e37527c63a1bdd1c639e2e178c9655edc
Ray Schireman <41241487+rschireman@users.noreply.github.com>
2024-06-24 21:39:01 -0500

    add per atom basis

--- database/connection.py
+++ database/connection.py
@@ -47,3 +47,43 @@ def query_basis(basis_set, _structures):
   
     return result
 
+# Uses st.cache_resource to only run once.
+@st.cache_resource
+def query_basis_individual_atoms(basis_set, atoms):
+    """
+    Query crystallographic basis data for a given basis set and atomic structures.
+
+    This function takes a basis set name and a dictionary of atomic structures as input,
+    and queries a database for crystallographic basis data. It returns a dictionary
+    that maps each unique atom in the structures to a list of basis data entries found
+    in the database for that atom and basis set.
+
+    Parameters:
+    basis_set (str): The name of the basis set to query.
+    atoms (str): List of atoms
+
+    Returns:
+    dict: A dictionary where the keys are unique atomic species found in the input
+          structures, and the values are lists of basis data entries retrieved from
+          the database for the specified basis set and atom. The structure of each
+          entry in the list depends on the database schema.
+
+    Example:
+    basis_set = "6-311G(d,p)"
+    structures = {
+        "structure1": StructureObject1,
+        "structure2": StructureObject2,
+        # ... (more structures)
+    }
+    basis_data = query_basis(basis_set, structures)
+    # Output: {'H': [BasisDataEntry1, BasisDataEntry2], 'O': [BasisDataEntry3, BasisDataEntry4], ...}
+    """
+    url: str = os.environ.get("SUPABASE_URL")
+    key: str = os.environ.get("SUPABASE_KEY")
+    supabase: Client = create_client(url, key)
+    result = {}
+    for atom in atoms:
+        response = supabase.table('CrystalBasisData').select('*').match({'element': atom, 'basis': basis_set}).execute()
+        result[atom] = list(response.data[0]['basis_data'])
+  
+    return result
--- pages/Per_atom_basis_data.py
+++ pages/Per_atom_basis_data.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import basis_set_exchange as bse
+from database.connection import  query_basis_individual_atoms
+import periodictable
 
 st.set_page_config(
         page_title="CRYSTAL Basis Sets",
@@ -8,7 +10,13 @@ st.set_page_config(
         initial_sidebar_state='expanded'
     )
 
+
+
 st.title('CRYSTAL Basis Sets')
+
+atom = st.selectbox("Atom", periodictable.elements[1::])
 
 all_basis_sets = bse.get_all_basis_names()
 user_basis_set = st.sidebar.selectbox("Basis Set", all_basis_sets, index=33)
+
+result = query_basis_individual_atoms(user_basis_set, atom)
